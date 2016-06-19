870d965f1b1c382b9993b216fbcc468a47681d53
Hanley Chan <hanleychan@gmail.com>
2016-06-19 15:39:20 -0700

    Added initial database migrations

---
+++ database/migrations/2016_06_19_220616_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+    }
+}
