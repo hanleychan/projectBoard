43d19f579d61fd97e7b3bc54e61fd86b40f47ff8
Hanley Chan <hanleychan@gmail.com>
2016-07-03 13:16:07 -0700

    Redirect to browse page after password reset

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'hanleychan@gmail.com', 'name' => 'Hanley Chan'],
 
     /*
     |--------------------------------------------------------------------------
